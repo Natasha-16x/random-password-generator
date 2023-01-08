fb710925fddcb7ad4b19812e6c2c78068c0510dd
Natasha Okparavero <tasha.okparavero@yahoo.com>
2023-01-08 09:00:29 +0000

    added function to prompt user for password options

--- starter/script.js
+++ starter/script.js
@@ -90,13 +90,22 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-
+  ("Choose a password length between 10 and 64 characters");
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+return arr[Math.floor(Math.random()*arr.length)];
 }
+
+var arr = {
+  specialCharacters: ['@','%','+','\\','/', "'", '!', '#', '$', '^', '?', ':', ',',')','(','}', '{', ']', '[', '~', '-', '_','.'],
+  numericCharacters: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  lowerCasedCharacters: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+  upperCasedCharacters: ['A','B','C','D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q','R','S','T','U','V','W','X','Y','Z']
+}
+
+console.log(getRandom(arr.specialCharacters));
 
 // Function to generate password with user input
 function generatePassword() {
